f89d09a8010b18f9d138ec3f70a9a1a851c10c7a
BogdanGP <bogdan.ganzha-pupyshev@patternica.com>
2021-12-01 00:46:44 +0300

    New Version Initial Commit

---
+++ College-Website-master/UmsaCollege/Models/IRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UmsaCollege.Models {
+    public interface IRepository {
+        IQueryable<Course> Courses { get; }
+
+        IQueryable<Student> Students { get; }
+
+        IQueryable<Student> GetStudents(int courseID);
+
+        Student GetStudentById(int courseID);
+        Student GetStudentByCode(string cod);
+        void SaveCourse(Course course);
+        void AddStudenttoCourse(int courseID,int studentid);
+        void Update(Course course);
+
+        Course GetById(int id);
+
+        void Delete(Course course);
+
+        void SaveStudent(Student student);
+    }
+}
